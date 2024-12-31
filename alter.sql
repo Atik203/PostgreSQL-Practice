c080c710f873dedd838cf17c504d4981bbbeed32
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-12-31 21:38:57 +0600

    added ALTER uses

--- alter.sql
+++ alter.sql
@@ -1,5 +1,27 @@
 
-SELECT * FROM person;
+-- show all fields in the table
+ select * from person;
 
+-- Add a column to the table
+ALTER TABLE person
+   ADD COLUMN email VARCHAR(255) DEFAULT 'default@gmail.com' NOT NULL;
 
-          
+-- Drop a column from the table
+ALTER TABLE person
+   DROP COLUMN email;
+
+-- rename a column
+ALTER TABLE person
+   RENAME COLUMN name TO full_name;
+
+-- change the data type of a column
+ALTER Table person
+    ALTER COLUMN full_name TYPE VARCHAR(120); 
+
+-- change the constraint of a column
+ALTER TABLE person
+    ALTER COLUMN full_name SET NOT NULL;
+
+-- remove the constraint of a column
+ALTER TABLE person
+    ALTER COLUMN full_name DROP NOT NULL;
