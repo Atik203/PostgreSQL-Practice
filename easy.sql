b19b3fa10aa228368514dff69f0c380214e3f951
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-17 01:05:31 +0600

    refactor SQL queries for clarity and organization

--- easy.sql
+++ easy.sql
@@ -11,10 +11,70 @@ FROM
     student;
 
 -- Show all courses with their credits.
--- Find all faculty members in the Computer Science department.
+SELECT
+    course_name,
+    credits
+FROM
+    course
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    -- Find all faculty members in the Computer Science department.
+
+-- store department_id Variables start with @ because they are user-defined variables
+SET @cs_department_id = (SELECT department_id FROM department WHERE dept_name = 'Computer Science');
+
+SELECT
+    *
+FROM
+    faculty
+WHERE
+    department_id = @cs_department_id;
+
+
+
+
+
+
 -- Show the names of students born after 2000-01-01.
+
+SELECT first_name, last_name FROM student WHERE dob > '2000-01-01';
+
+
+
+
+
+
 -- Retrieve all departments.
+
+SELECT * FROM department;
+
+
+
+
+
 -- Get all enrollments of student with student_id = 3.
+SELECT * from enrollment WHERE student_id = 3;
+
+
+
+
+
+
 -- List all female students.
 -- Show all courses offered by department 2.
 -- Display all faculty ordered by last name.
