5acb042154089db1beae6543ef3fb6c1405d9aaf
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-08-17 01:22:27 +0600

    refactor SQL queries for improved readability and organization; add total student count query

--- easy.sql
+++ easy.sql
@@ -16,66 +16,70 @@ SELECT
     credits
 FROM
     course
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     -- Find all faculty members in the Computer Science department.
-
--- store department_id Variables start with @ because they are user-defined variables
-SET @cs_department_id = (SELECT department_id FROM department WHERE dept_name = 'Computer Science');
-
 SELECT
     *
 FROM
     faculty
 WHERE
-    department_id = @cs_department_id;
-
-
-
-
-
+    department_id = (
+        SELECT
+            department_id
+        FROM
+            department
+        WHERE
+            dept_name = 'Computer Science'
+    );
 
 -- Show the names of students born after 2000-01-01.
-
-SELECT first_name, last_name FROM student WHERE dob > '2000-01-01';
-
-
-
-
-
+SELECT
+    first_name,
+    last_name
+FROM
+    student
+WHERE
+    dob > '2000-01-01';
 
 -- Retrieve all departments.
-
-SELECT * FROM department;
-
-
-
-
+SELECT
+    *
+FROM
+    department;
 
 -- Get all enrollments of student with student_id = 3.
-SELECT * from enrollment WHERE student_id = 3;
-
-
-
-
-
+SELECT
+    *
+from
+    enrollment
+WHERE
+    student_id = 3;
 
 -- List all female students.
+SELECT
+    *
+from
+    student
+WHERE
+    gender = 'F';
+
 -- Show all courses offered by department 2.
+select
+    *
+from
+    course
+WHERE
+    department_id = 2;
+
 -- Display all faculty ordered by last name.
+SELECT
+    *
+from
+    faculty
+ORDER BY
+    last_name;
+
 -- Count the total number of students.
+SELECT
+    COUNT(*) AS total_students
+FROM
+    student;
